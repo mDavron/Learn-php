a9c294ccdddb2f3a97bc4eb5e6ce14a6586220b4
Davron01~ <d.mirabdullayev@mail.ru>
2024-10-25 20:56:34 +0500

    php increment

--- index.php
+++ index.php
@@ -9,28 +9,45 @@
 
 <body>
   <?php
-  /* comments */
-  $phone = 998932743776;
-  $keys = 'String';
-  $money = '123.45';
-  echo "$phone  <br>";
-  echo "$keys  <br>";
-  echo "$money  <br>";
-  echo "Hello Php <br>";
-  print $keys;
+    /* comments */
+    $phone = 998932743776;
+    $keys = 'String';
+    $money = '123.45';
+    echo "$phone  <br>";
+    echo "$keys  <br>";
+    echo "$money  <br>";
+    echo "Hello Php <br>";
+    print $keys;
 
-  echo '<h1>';
-  $z = 'Heading1';
-  echo $z . '</h1>';
-  $value = 2;
+    echo '<h1>';
+    $z = 'Heading1';
+    echo $z . '</h1>';
+    $value = 2;
   ?>
 
   <input type="text" value="<?php echo $value?>"> <br><br>
   <input type="text" value="<?= $value+1?>"> <br><br>
 
   <?php
-  $basket = array('12',45,123.45,'keys');
-  var_dump($basket);
+    $basket = array('12',45,123.45,'keys');
+    echo $basket[1];
+    var_dump($basket);
+  ?> <br><br>
+  <?php
+  $basket_array = array(
+    'phone' => 998932743776,
+    'keys' => 'String',
+    'money' => '123.45',
+    'name' => "Hello Php <br>"
+  );
+    var_dump( $basket_array);
+    $money1 = $basket_array['money'];
+    $money1 = $money1+10;
+    $money1++;
+    $money1+=10;
+    echo 'Money' .' : '. $money1;
+    /* or */
+    echo "Money : $money1";
   ?>
 </body>
 
